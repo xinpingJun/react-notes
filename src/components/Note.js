11368435f31f0757071e9e0bf8b58773dcbb4a70
心平菌 <903617610@qq.com>
2018-06-06 14:02:18 +0800

    编辑未完成

---
+++ src/components/Note.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import Editor from "./Editor";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import {loadCollection,db} from '../database'
+import _ from "loadsh";
+moment.locale("zh-CN");
+class Note extends Component {
+  state = {
+    entity: this.props.entity,
+    body: this.props.entity.body,
+    updated: this.props.entity.meta.updated || this.props.entity.created,
+    open: false
+  };
+  updated() {
+    return moment(this.state.updated).fromNow();
+  }
+  header() {
+    return _.truncate(this.state.body, { length: 30 } ) || " 新建笔记 ";
+  }
+  words() {
+    return this.state.body.length;
+  }
+
+  toggle= ()=>{
+    this.setState((prevState)=>{
+      return {
+        open:!prevState.open
+      }
+    })
+  }
+  updateEntity = (event) =>{
+    const _body = event.targer.value
+    this.setState({
+      body:_body
+    })
+    loadCollection('notes')
+    .then((collection)=>{
+      const entity = this.state.entity
+      entity.body = _body
+      collection.updated(entity)
+      db.saveDatabase()
+    })
+  }
+  render() {
+    return (
+      <div className="item">
+        <div className="meta">{this.updated()}</div>
+        <div className="content">
+          <div className="header" onClick={this.toggle}>{this.header()}</div>
+          <div className="extra">
+            {this.state.open && <Editor  entity={this.state.entity} updateEntity={this.updateEntity} />}
+            {this.words()}字
+            {this.state.open && <i className="right floated trash  icon" />}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Note;
